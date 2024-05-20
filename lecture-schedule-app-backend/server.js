33489eacd8a365ac034a0153a740c0c04fd243e1
Rithik <jainrithik2000@gmail.com>
2024-05-21 01:36:13 +0530

    updated

--- lecture-schedule-app-backend/server.js
+++ lecture-schedule-app-backend/server.js
@@ -24,12 +24,6 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(cors());
 
-app.use("*",(req,res, next) => {
-  console.log("json upar");
-  console.log(req.body);
-next();
-})
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
